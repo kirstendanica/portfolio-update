1ffcdf55bec3756973481f66c170619197447f6e
kirsten d <kirdanic@gmail.com>
2023-10-12 23:36:29 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+// Greetings array in different languages
+const greetings = [
+    "Bonjour, bienvenue",
+    "Hola, bienvenido",
+    "Здравствуйте, добро пожаловать",
+    "你好，欢迎",
+    "Hej, välkomna",
+    "Hei, tervetuloa",
+    "Ciao, benvenuto",
+    "مرحبا أهلا وسهلا"
+];
+
+const greetingElement = document.getElementById("greeting");
+let index = 0;
+
+function changeGreeting() {
+    greetingElement.style.opacity = "0";
+
+     setTimeout(() => {
+        greetingElement.textContent = greetings[index];
+        greetingElement.style.opacity = "1";
+
+        index++;
+    
+        if (index === greetings.length) {
+            index = 0;
+        }
+
+     }, 500);
+}
+
+// Change lang greeting after several seconds
+setInterval(changeGreeting, 4000);
+
+// Navigation for smaller screens
+function toggleNav() {
+    const nav = document.querySelector("nav");
+    nav.classList.toggle("active");
+  }
+  
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+  
+      document.querySelector(this.hash).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
