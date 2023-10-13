4177329166e3d0c47b08b55ed479fc1bb8f895a1
kirsten d <kirdanic@gmail.com>
2023-10-13 03:58:56 -0500

    Fix some small errors in syntax, add photo for About Me section, optimize SEO

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
 // Greetings array in different languages
 const greetings = [
-    "Bonjour, bienvenue",
-    "Hola, bienvenido",
-    "Здравствуйте, добро пожаловать",
-    "你好，欢迎",
-    "Hej, välkomna",
-    "Hei, tervetuloa",
-    "Ciao, benvenuto",
-    "مرحبا أهلا وسهلا"
+  "Bonjour, bienvenue",
+  "Hola, bienvenido",
+  "Здравствуйте, добро пожаловать",
+  "你好，欢迎",
+  "Hej, välkomna",
+  "Hei, tervetuloa",
+  "Ciao, benvenuto",
+  "مرحبا أهلا وسهلا",
 ];
 
 const greetingElement = document.getElementById("greeting");
 let index = 0;
 
 function changeGreeting() {
-    greetingElement.style.opacity = "0";
+  greetingElement.style.opacity = "0";
 
-     setTimeout(() => {
-        greetingElement.textContent = greetings[index];
-        greetingElement.style.opacity = "1";
+  setTimeout(() => {
+    greetingElement.textContent = greetings[index];
+    greetingElement.style.opacity = "1";
 
-        index++;
-    
-        if (index === greetings.length) {
-            index = 0;
-        }
+    index++;
 
-     }, 500);
+    if (index === greetings.length) {
+      index = 0;
+    }
+  }, 500);
 }
 
 // Change lang greeting after several seconds
@@ -34,17 +33,26 @@ setInterval(changeGreeting, 4000);
 
 // Navigation for smaller screens
 function toggleNav() {
-    const nav = document.querySelector("nav");
-    nav.classList.toggle("active");
-  }
-  
+  const nav = document.querySelector("nav");
+  nav.classList.toggle("active");
+}
+
 // Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(link => {
-    link.addEventListener("click", function(e) {
-      e.preventDefault();
-  
-      document.querySelector(this.hash).scrollIntoView({
-        behavior: "smooth"
-      });
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.hash).scrollIntoView({
+      behavior: "smooth",
     });
   });
+});
+
+// Darkmode
+const darkModeInput = document.getElementById("dark-mode-input");
+const bodyElement = document.querySelector("body");
+
+darkModeInput.addEventListener("change", () => {
+  bodyElement.classList.toggle("dark-mode");
+});
+
